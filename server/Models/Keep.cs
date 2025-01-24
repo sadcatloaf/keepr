9776800894265165d05dd2b46e2ab1f9b0d50f12
melanie <mjharv@hotmail.com>
2025-01-24 12:31:23 -0700

    created models

--- server/Models/Keep.cs
+++ server/Models/Keep.cs
@@ -13,6 +13,9 @@ public class Keep
     public string Img { get; set; }
     public int Views { get; set; }
     public string CreatorId { get; set; }
+
+    public Account Creator { get; set; }
+    public int Kept { get; set; }
 }
 
 // = DateTime.UtcNow; --> formatted datetime
