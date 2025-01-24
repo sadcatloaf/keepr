98980fc2b051e5ce95964620d48ca92b63e861b3
melanie <mjharv@hotmail.com>
2025-01-24 11:59:02 -0700

    add tables

---
+++ server/Models/Keep.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace keepr.Models;
+
+
+public class Keep
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Img { get; set; }
+    public int Views { get; set; }
+    public string CreatorId { get; set; }
+}
+
+// = DateTime.UtcNow; --> formatted datetime
+
+
+
