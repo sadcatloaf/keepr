a6726ed8290aec4719d15e96f3ae61be13ad136c
melanie <mjharv@hotmail.com>
2025-01-24 16:31:47 -0700

    removed useless info

--- server/Repositories/VaultsRepository.cs
+++ server/Repositories/VaultsRepository.cs
@@ -76,13 +76,3 @@ public class VaultsRepository
     }
 }
 
-// t; set; }
-//     public DateTime CreatedAt { get; set; }
-//     public DateTime UpdatedAt { get; set; }
-//     public string Name { get; set; }
-//     public string Description { get; set; }
-//     public string Img { get; set; }
-//     public bool? IsPrivate { get; set; }
-//     public string CreatorId { get; set; }
-//     public Account Creator { get; set; }
-// }
